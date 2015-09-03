b034f7f0b99ea5921260865f008b461505da8040
Ben Cochrane <ben.cochrane2112@gmail.com>
2015-09-03 03:01:35 -0400

    Implemented a basic version of the render function

--- src/renderer.jsx
+++ src/renderer.jsx
@@ -1,7 +1,8 @@
+import { createElement } from 'react'
 
-import ComponentRegistry from './registry'
-import DomRegistry from './domRegistry'
-
-export function renderData(data) {
-  
+export function renderData({ props, children, tagName }) {
+  let kids = children.length > 0 ? children.map(renderData) : [];
+  // TODO: Support lookups in the composite component registry
+  let tagFn = tagName[0] === tagName[0].toLowerCase() ? tagName : null;
+  return createElement(tagFn, props, ...kids);
 }
