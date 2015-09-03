c49ef538e78a2e3e6dc2f12b6519ccb4a2cb45f1
Ben Cochrane <ben.cochrane2112@gmail.com>
2015-09-03 03:19:57 -0400

    Adds a React rendering component that uses the renderData function

--- src/renderer.jsx
+++ src/renderer.jsx
@@ -1,6 +1,10 @@
-import { createElement } from 'react'
+import { Component, createElement, PropTypes } from 'react'
 
-export function renderData({ props, children, tagName }, registry) {
+export function renderData(data, registry) {
+  if (typeof data === 'string' || data instanceof String) {
+    return data;
+  }
+  let { children, tagName, props } = data;
   let kids = children.length > 0 ? children.map(renderData) : [];
   let tagFn = tagName[0] == tagName[0].toLowerCase() ?
       tagName
@@ -8,3 +12,23 @@ export function renderData({ props, children, tagName }, registry) {
       registry[tagName];
   return createElement(tagFn, props, ...kids);
 }
+
+export class RmcRenderer extends Component {
+  render() {
+    return renderData(this.props.data, this.context.registry);
+  }
+}
+
+RmcRenderer.propTypes = {
+  data: PropTypes.shapeOf({
+    tagName: PropTypes.string.isRequired,
+    props: PropTypes.object.isRequired,
+    children: PropTypes.arrayOf(PropTypes.oneOf([
+      PropTypes.string, PropTypes.object
+    ])).isRequired
+  })
+};
+
+RmcRenderer.contextTypes = {
+  registry: PropTypes.object
+};
