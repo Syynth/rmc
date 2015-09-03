c675be66f6bbe9760c7c03b6b2bf4147cd85f67b
Ben Cochrane <ben.cochrane2112@gmail.com>
2015-09-02 21:01:41 -0400

    Adds a test suite for registry

---
+++ __tests__/registry-test.js
@@ -0,0 +1,69 @@
+jest.dontMock('../src/registry.js');
+
+const ComponentRegistry = require('../src/registry.js');
+
+describe('ComponentRegistry', () => {
+
+  describe('api', () => {
+
+    it('should have a register method', () => {
+      let inst = new ComponentRegistry();
+      expect(inst.register).toBeDefined();
+    });
+
+    it('should have a deregister method', () => {
+      let inst = new ComponentRegistry();
+      expect(inst.deregister).toBeDefined();
+    });
+
+  });
+
+  describe('component registration', () => {
+
+    it('should add registered components as properties on the registry', () => {
+      let inst = new ComponentRegistry();
+      expect(inst.someComponent).not.toBeDefined();
+      inst.register('someComponent', () => 0);
+      expect(inst.someComponent).toBeDefined();
+    });
+
+  });
+
+  describe('component deregistration', () => {
+
+    it('should remove registered components as properties on the registry', () => {
+      let inst = new ComponentRegistry();
+      inst.register('someComponent', () => 0);
+      expect(inst.someComponent).toBeDefined();
+      inst.deregister('someComponent');
+      expect(inst.someComponent).not.toBeDefined();
+    });
+
+  })
+
+  describe('freezing behavior', () => {
+
+    it('should remove the register/deregister methods when constructed', () => {
+      let inst = new ComponentRegistry(null, true);
+      expect(inst.register).not.toBeDefined();
+      expect(inst.deregister).not.toBeDefined();
+    });
+
+    it('should prevent extensions if freeze is passed to the constructor', () => {
+      expect(Object.isExtensible(new ComponentRegistry(null, true)))
+        .toEqual(false);
+    });
+
+    it('should seal registry if freeze is passed to the constructor', () => {
+      expect(Object.isSealed(new ComponentRegistry(null, true)))
+        .toEqual(true);
+    });
+
+    it('should freeze registry if freeze is passed to the constructor', () => {
+      expect(Object.isFrozen(new ComponentRegistry(null, true)))
+        .toEqual(true);
+    });
+
+  });
+
+});
